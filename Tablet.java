38ef30c492b477208bdd23b927be8d8d8cbfea32
khooyenni <73241253+khooyenni@users.noreply.github.com>
2021-04-05 23:37:10 +0800

    Update and rename KitchenAppliances2.java to Tablet.java

---
+++ Tablet.java
@@ -0,0 +1,22 @@
+package Example1;
+
+public class Tablet {
+	
+	String brand = "Huawei";
+	String model = "MatePad Pro";
+	String colour = "GRAY";
+	
+	void call(){
+		System.out.println("Tablet can make a call.");
+	}
+	
+	void youtube(){
+		System.out.println("Tablet can watch youtube.");
+	}
+	
+	void notification(){
+		System.out.println("Tablet can receive notification.");
+	}
+
+
+}
