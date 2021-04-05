cc0e1016c2417f43d15df4500909e80bedbb4b03
khooyenni <73241253+khooyenni@users.noreply.github.com>
2021-04-05 12:51:59 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,23 +5,23 @@ public class Main {
 
 public static void main(String[] args) {
 		
-		KitchenAppliances1 Breakfastmaker = new KitchenAppliances1();
-		System.out.println("The Brand of Breakfastmaker: " + Breakfastmaker.brand);
-		System.out.println("The Voltage of Breakfastmaker: " + Breakfastmaker.voltage);
-		System.out.println("The Oven Capacity of Breakfastmaker: " + Breakfastmaker.ovenCapacity);
-		Breakfastmaker.bake();
-		Breakfastmaker.toast();
-		Breakfastmaker.fry();
+		KitchenAppliances1 Sunpentown = new KitchenAppliances1();
+		System.out.println("The Brand of Breakfastmaker: " + Sunpentown.brand);
+		System.out.println("The Voltage of Breakfastmaker: " + Sunpentown.voltage);
+		System.out.println("The Oven Capacity of Breakfastmaker: " + Sunpentown.ovenCapacity);
+		Sunpentown.bake();
+		Sunpentown.toast();
+		Sunpentown.fry();
 		
 		System.out.println();
 		
-		KitchenAppliances2 MicrowaveOven = new KitchenAppliances2();
-		System.out.println("The Brand of Microwave Oven: " + MicrowaveOven.brand);
-		System.out.println("The Model of Microwave Oven: " + MicrowaveOven.model);
-		System.out.println("The Oven Capacity of Microwave Oven: " + MicrowaveOven.ovenCapacity);
-		MicrowaveOven.bake();
-		MicrowaveOven.grill();
-		MicrowaveOven.defrost();
+		KitchenAppliances2 Sharp = new KitchenAppliances2();
+		System.out.println("The Brand of Microwave Oven: " + Sharp.brand);
+		System.out.println("The Model of Microwave Oven: " + Sharp.model);
+		System.out.println("The Oven Capacity of Microwave Oven: " + Sharp.ovenCapacity);
+		Sharp.bake();
+		Sharp.grill();
+		Sharp.defrost();
 		
 
 	}
